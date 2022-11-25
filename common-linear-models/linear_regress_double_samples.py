36e627719ebd534c1b3152d9ea5eb554195038e2
TowardsAurora <90918138+TowardsAurora@users.noreply.github.com>
2022-11-25 22:03:09 +0800

    Update linear_regress_double_samples.py

--- common-linear-models/linear_regress_double_samples.py
+++ common-linear-models/linear_regress_double_samples.py
@@ -1,76 +1,126 @@
 import numpy as np
+from matplotlib import pyplot as plt
 
-data = np.array([
-    [1, 3],
-    [2, 5],
-    [3, 9],
-    [4, 11],
-    [5, 14.5],
-    [6, 18.3],
-    [7, 21.2],
-    [8, 25],
-    [9, 26.6],
-    [10, 31]
-])
-
-
-print(data)
-
-train_len = 7
-train_data = data[0:7]
-test_data = data[7:]
-
-print(train_data)
-print(test_data)
-
-x = train_data[:, 0]
-y = train_data[:, 1]
-print("x= ", x, "\n", "y= ", y)
-
-sum_x = 0
-sum_xx = 0
-for x_i in x:
-    sum_x += x_i
-    sum_xx += x_i * x_i
-avg_x = sum_x / len(x)
-avg_xx = sum_xx / len(x)
-
-sum_y = 0
-for y_i in y:
-    sum_y += y_i
-avg_y = sum_y / len(y)
-
-print("avg_x= ", avg_x, "\navy_y= ", avg_y)
-
-sum_xy = 0
-for x_i, y_i in train_data:
-    sum_xy += x_i * y_i
-    # print("x_i= ",x_i,"y_i=",y_i)
-avg_xy = sum_xy / len(train_data)
-
-print("avg_xy=", avg_xy)
-print("avg_xx=", avg_xx)
-w1 = (avg_xy - avg_x * avg_y) / (avg_xx - avg_x * avg_x)
-w0 = avg_y - w1 * avg_x
-print("w0 = ", w0, "\nw1 = ", w1)
-print("y={}+{}x".format(w0, w1))
+"""
+这里我们讨论二元情况
+即: y_pre=kx+b
+target: minimize sum (y - y_pre)^2
+min sum (y-(kx+b))^2
+min sum (y^2-2*y*(kx+b)+(kx+b)^2)
+分别对 k,b求导 令其为0
+Partial y/k: k*(x^2).avg =b*x.avg-(y*x).avg 
+Partial y/b: b=k*x.avg-y
+solve:
+k=((x*y).avg-x.avg*y.avg)
+b=k*x.avg-y
+"""
 
 
-def predict(test):
-    prediction = w0 + w1 * test
-    return prediction
+def spilt_date(data, spilt_point):
+    train_data = data[0:spilt_point]
+    test_data = data[spilt_point:len(data)]
+    return train_data, test_data
 
 
-pre = predict(test_data[:, 0])
-print("true = ", test_data[:, 1])
-print("prediction = ", pre)
+def create_data(data):
+    X = data[:, 0]
+    Y = data[:, 1]
+    return X, Y
 
-array = np.array([pre, test_data[:, 1]]).T
-print(array)
-test_len = len(test_data)
 
-count = 0
-for t, p in array:
-    if abs(t - p) < 1:
-        count += 1
-print("在误差为1的情况下预测的准确率为：{}%".format(count / test_len * 100))
+class linear_regression():
+    def __init__(self):
+        self.k = None
+        self.b = None
+
+    @staticmethod
+    def calculate_avg_x(X):
+        sum = 0
+        for x in X:
+            sum += x
+        avg_x = sum / (len(X))
+        return avg_x
+
+    @staticmethod
+    def calculate_avg_y(Y):
+        sum = 0
+        for y in Y:
+            sum += y
+        avg_y = sum / (len(Y))
+        return avg_y
+
+    @staticmethod
+    def calculate_avg_xx(X):
+        sum = 0
+        for x in X:
+            sum += x * x
+        avg_xx = sum / (len(X))
+        return avg_xx
+
+    @staticmethod
+    def calculate_avg_xy(X, Y):
+        sum = 0
+        for x, y in zip(X, Y):
+            sum += x * y
+        avg_xy = sum / (len(X))
+        return avg_xy
+
+    def train(self, X, Y):
+        avg_xy = self.calculate_avg_xy(X, Y)
+        avg_xx = self.calculate_avg_xx(X)
+        avg_x = self.calculate_avg_x(X)
+        avg_y = self.calculate_avg_y(Y)
+        self.k = (avg_xy - avg_x * avg_y) / (avg_xx - avg_x * avg_x)
+        self.b = avg_y - self.k * avg_x
+        return self.k, self.b
+
+    def predict_y(self, X):
+        Y = []
+        for x in X:
+            y = k * x + b
+            Y.append(y)
+        return Y
+
+    def test(self, X, Y, bias):
+        count = 0
+        predict_y = self.predict_y(X)
+        for y, y_pre in zip(Y, predict_y):
+            if abs(y - y_pre) < bias:
+                count += 1
+        acc = count / len(X)
+        return acc
+
+
+if __name__ == '__main__':
+    data = np.array([
+        [1, 3],
+        [2, 5],
+        [3, 9],
+        [4, 11],
+        [5, 14.5],
+        [6, 18.3],
+        [7, 21.2],
+        [8, 25],
+        [9, 26.6],
+        [10, 31]
+    ])
+    model = linear_regression()
+    length = len(data)
+    X, Y = create_data(data)
+    print("data length:", length)
+    spilt_point = 7
+    train_data, test_data = spilt_date(data, spilt_point)
+    train_X, train_Y = create_data(train_data)
+    print("train:", train_X, train_Y)
+    test_X, test_Y = create_data(test_data)
+    print("test:", test_X, test_Y)
+    k, b = model.train(train_X, train_Y)
+    model_predict_y = model.predict_y(test_X)
+    print("predict in test :", model_predict_y)
+    bias = 1
+    accuracy = model.test(test_X, model_predict_y, bias)
+    print("under bias {},the accuracy is {}%".format(bias, accuracy * 100))
+    plt.scatter(train_X, train_Y, marker='.', color='red', s=40, label='Train')
+    plt.scatter(test_X, test_Y, marker='.', color='blue', s=40, label='Test')
+    plt.plot(X, model.predict_y(X), linewidth=1, color='green')
+    plt.show()
